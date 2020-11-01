4c497e250a02caea1949339be18794b27be22e24
Abhishek  Kumar <abhishek.111002@gmail.com>
2020-11-01 09:53:48 +0800

    intial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CourseController } from './course/course.controller';
+
+
+@Module({
+  imports: [],
+  controllers: [AppController, CourseController],
+  providers: [AppService],
+})
+export class AppModule {}
